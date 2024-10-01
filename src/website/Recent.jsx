f48b76490b78fd42db6381fd187be3f43c9dfb37
monmonplaza <monmon.plaza@gmail.com>
2024-10-01 22:36:30 +0800

    123

--- src/website/Recent.jsx
+++ src/website/Recent.jsx
@@ -5,18 +5,60 @@ const Recent = () => {
   return (
     <section className="py-20 bg-white">
       <div className="container">
-        <div className="grid grid-cols-[2fr_1fr] gap-10">
+        <div className="grid grid-cols-[3.5fr_1fr] gap-10">
           <div>
             <h3>Recent Recipes</h3>
 
-            <div className="card flex items-center">
-              <figure>
-                <img src="https://via.placeholder.com/300x250" alt="" />
+            <div className="card flex items-center mb-10">
+              <figure className="basis-3/4">
+                <img
+                  src="https://via.placeholder.com/350x200"
+                  alt=""
+                  className="w-[400px] h-[300px] object-cover"
+                />
               </figure>
               <article className="p-10">
                 <MetaInfo tags="Healthy" time="10mins" />
-                <h3>Serving safe food it;s not an option it's an obligation</h3>
-                <p>
+                <h3>Serving safe food it's not an option it's an obligation</h3>
+                <p className="mb-0">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Consectetur distinctio ea voluptate ratione cumque deleniti
+                  quo nesciunt, accusamus perferendis eum.
+                </p>
+              </article>
+            </div>
+
+            <div className="card flex items-center mb-10">
+              <figure className="basis-3/4">
+                <img
+                  src="https://via.placeholder.com/350x200"
+                  alt=""
+                  className="w-[400px] h-[300px] object-cover"
+                />
+              </figure>
+              <article className="p-10">
+                <MetaInfo tags="Healthy" time="10mins" />
+                <h3>Serving safe food it's not an option it's an obligation</h3>
+                <p className="mb-0">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Consectetur distinctio ea voluptate ratione cumque deleniti
+                  quo nesciunt, accusamus perferendis eum.
+                </p>
+              </article>
+            </div>
+
+            <div className="card flex items-center mb-10">
+              <figure className="basis-3/4">
+                <img
+                  src="https://via.placeholder.com/350x200"
+                  alt=""
+                  className="w-[400px] h-[300px] object-cover"
+                />
+              </figure>
+              <article className="p-10">
+                <MetaInfo tags="Healthy" time="10mins" />
+                <h3>Serving safe food it's not an option it's an obligation</h3>
+                <p className="mb-0">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Consectetur distinctio ea voluptate ratione cumque deleniti
                   quo nesciunt, accusamus perferendis eum.
